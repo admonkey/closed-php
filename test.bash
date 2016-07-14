31923906472acd53b1842fd69d7c7e9dd246ab8e
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-14 17:44:20 -0500

    remove unecessary code

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 for testfile in $(ls tests); do
-  $(./closed-php.bash tests/$testfile)
+  ./closed-php.bash tests/$testfile
   closed=$?
   if [ $closed -eq 1 -a "true"  == ${testfile##*.} ] || 
      [ $closed -eq 0 -a "false" == ${testfile##*.} ]; then
